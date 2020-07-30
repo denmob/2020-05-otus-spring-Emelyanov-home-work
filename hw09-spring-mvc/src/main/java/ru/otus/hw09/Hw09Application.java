2a6d815e6c247b522c4e5b1af00d92c6eba08825
Denis <mwarez@yahoo.com>
2020-07-30 19:22:06 +0300

    added simple localization

--- hw09-spring-mvc/src/main/java/ru/otus/hw09/Hw09Application.java
+++ hw09-spring-mvc/src/main/java/ru/otus/hw09/Hw09Application.java
@@ -3,10 +3,12 @@ package ru.otus.hw09;
 import lombok.SneakyThrows;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
 @EnableMongoRepositories
+@EnableConfigurationProperties
 public class Hw09Application {
 
   @SneakyThrows
